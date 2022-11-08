820f11471a5ddc1861f5b936b84a2d1e560bd189
ambar <ambar@source.ag>
2022-11-08 21:58:42 +0100

    feat: check vertical lines correctness of sudoku grid at any time

--- internal/sudokuGrid.go
+++ internal/sudokuGrid.go
@@ -15,8 +15,8 @@ func (sg SudokuGrid) GetRowOfCubes() [3]*[3]SingleCube {
 
 func (sg SudokuGrid) CheckCompleteness() (bool, error) {
 	cubeCompleteness := true
-	verticalCompleteness := true
 	horizontalCompleteness := true
+	verticalCompleteness := true
 
 	for _, cubeRow := range sg.GetRowOfCubes() {
 		for _, v := range cubeRow {
@@ -29,6 +29,14 @@ func (sg SudokuGrid) CheckCompleteness() (bool, error) {
 	}
 
 	for _, v := range sg.GetHorizontalLines() {
+		tempHorizontalCompleteness, _ := OneToNine(v)
+		fmt.Println(v, tempHorizontalCompleteness)
+		if !tempHorizontalCompleteness {
+			horizontalCompleteness = false
+		}
+	}
+
+	for _, v := range sg.GetVerticalLines() {
 		tempVerticalCompleteness, _ := OneToNine(v)
 		fmt.Println(v, tempVerticalCompleteness)
 		if !tempVerticalCompleteness {
@@ -36,7 +44,7 @@ func (sg SudokuGrid) CheckCompleteness() (bool, error) {
 		}
 	}
 
-	return cubeCompleteness && verticalCompleteness && horizontalCompleteness, nil
+	return cubeCompleteness && horizontalCompleteness && verticalCompleteness, nil
 
 }
 
@@ -63,6 +71,36 @@ func (sg SudokuGrid) GetHorizontalLines() [][]int {
 		bottomRow = nil
 	}
 	return horizontalLines
+}
+
+func (sg SudokuGrid) GetVerticalLines() [][]int {
+
+	var verticalLines [][]int
+	verticalSlices := make(map[int][]int)
+
+	for i := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8} {
+		verticalSlices[i] = []int{}
+	}
+
+	for _, cubeRow := range sg.GetRowOfCubes() {
+		verticalSlices[0] = append(verticalSlices[0], []int{cubeRow[0].TopRowCube[0], cubeRow[0].MiddleRowCube[0], cubeRow[0].BottomRowCube[0]}...)
+		verticalSlices[1] = append(verticalSlices[1], []int{cubeRow[0].TopRowCube[1], cubeRow[0].MiddleRowCube[1], cubeRow[0].BottomRowCube[1]}...)
+		verticalSlices[2] = append(verticalSlices[2], []int{cubeRow[0].TopRowCube[2], cubeRow[0].MiddleRowCube[2], cubeRow[0].BottomRowCube[2]}...)
+
+		verticalSlices[3] = append(verticalSlices[3], []int{cubeRow[1].TopRowCube[0], cubeRow[1].MiddleRowCube[0], cubeRow[1].BottomRowCube[0]}...)
+		verticalSlices[4] = append(verticalSlices[4], []int{cubeRow[1].TopRowCube[1], cubeRow[1].MiddleRowCube[1], cubeRow[1].BottomRowCube[1]}...)
+		verticalSlices[5] = append(verticalSlices[5], []int{cubeRow[1].TopRowCube[2], cubeRow[1].MiddleRowCube[2], cubeRow[1].BottomRowCube[2]}...)
+
+		verticalSlices[6] = append(verticalSlices[6], []int{cubeRow[2].TopRowCube[0], cubeRow[2].MiddleRowCube[0], cubeRow[2].BottomRowCube[0]}...)
+		verticalSlices[7] = append(verticalSlices[7], []int{cubeRow[2].TopRowCube[1], cubeRow[2].MiddleRowCube[1], cubeRow[2].BottomRowCube[1]}...)
+		verticalSlices[8] = append(verticalSlices[8], []int{cubeRow[2].TopRowCube[2], cubeRow[2].MiddleRowCube[2], cubeRow[2].BottomRowCube[2]}...)
+	}
+
+	for _, v := range verticalSlices {
+		verticalLines = append(verticalLines, v)
+	}
+
+	return verticalLines
 }
 
 func (sg SudokuGrid) PrintGrid() {
